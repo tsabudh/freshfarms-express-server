cae9c2edd5a7af58b1c0a3b61fcc0e1ef297b6de
Sabudh Bahadur Thapa <tsabudh@gmail.com>
2023-05-23 19:04:26 +0545

    customer added to transaction model

--- model/transactionModel.js
+++ model/transactionModel.js
@@ -1,31 +1,45 @@
 const mongoose = require("mongoose");
 const Product = require("../model/productModel");
+const Customer = require("../model/customerModel");
 
-const transactionSchema = new mongoose.Schema({
-  issuedTime: { type: Date, default: Date.now(), immutable: true },
-  items: [
-    {
-      priceThen: {
-        type: Number,
-      },
-      productName: {
-        type: String,
-      },
-      productId: {
-        type: mongoose.Types.ObjectId,
-        ref: "Product",
-      },
-      quantity: { type: Number, default: 1 },
+const transactionSchema = new mongoose.Schema(
+  {
+    issuedTime: { type: Date, default: Date.now(), immutable: true },
+    customer: {
+      id: { type: mongoose.Types.ObjectId, ref: "Customer", default: null },
+      name: { type: String, default: "Unregistered Customer" },
     },
-  ],
-});
+    items: [
+      {
+        priceThen: {
+          type: Number,
+        },
+        productName: {
+          type: String,
+        },
+        productId: {
+          type: mongoose.Types.ObjectId,
+          ref: "Product",
+        },
+        quantity: { type: Number, default: 1 },
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
 
 transactionSchema.pre("save", async function (next) {
   let products = await Product.find().select("name _id price");
   console.log(products);
   this.items.forEach((item) => {
     if (item.productId) {
-      let product1 = products.find((product) => product._id.toString() == item.productId);
+      let product1 = products.find(
+        (product) => product._id.toString() == item.productId
+      );
       if (!product1)
         throw new Error(`Product with id "${item.productId}" not found.`);
 
@@ -43,7 +57,15 @@ transactionSchema.pre("save", async function (next) {
       item.priceThen = product2.price;
     } else throw new Error("provide product name or id");
   });
+  next();
 });
+
+
+transactionSchema.pre('save', async function(next){
+  if(this.customer.id){
+    this.customer.name = await Customer.findById(this.customer.id).name;
+  }
+})
 
 const Transaction = mongoose.model("Transaction", transactionSchema);
 
