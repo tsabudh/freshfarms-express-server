c04697462eb9ed897e2b2229dec1ea7be6913b8b
Sabudh Bahadur Thapa <tsabudh@gmail.com>
2023-05-27 23:00:29 +0545

    filters pricerange, itemsarray, no. of items, variety of items, date added

--- controller/transactionController.js
+++ controller/transactionController.js
@@ -27,37 +27,100 @@ const getAllTransactions = async (req, res, next) => {
   try {
     const limit = req.query.limit;
     const product = req.query.product;
-    let filter = {
-      _id: { $eq: "646cdb8064e7d5835ff90552" },
-    };
-    let date1 = new Date("2023-05-23T15:24:13.295+00:00");
-    let date2 = new Date("2023-05-26T15:24:13.295+00:00");
-    let quantity1 = 4;
-    let quantity2 = 5;
-    let itemLowerRange = 2;
-    let itemUpperRange = 4;
-    // const mongooseQuery = Transaction.find(filter).select("+cost");
-    const mongooseQuery = Transaction.aggregate([
+
+    const aggregationPipeline = [
       {
-        $unwind: "$items",
-      },
-      // {
-      //   $match:{
-      //     'items.quantity':1
-      //   }
-      // }
-      {
-        $project: {
-          total: { $sum: "$items.quantity" },
+        $addFields: {
+          transactionItemsQuantity: {
+            $reduce: {
+              input: "$items",
+              initialValue: 0,
+              in: {
+                $add: ["$$value", "$$this.quantity"],
+              },
+            },
+          },
+          transactionAmount: {
+            $reduce: {
+              input: "$items",
+              initialValue: 0,
+              in: {
+                $add: ["$$value", "$$this.priceThen"],
+              },
+            },
+          },
         },
       },
-      {
-        $group: {
-          _id: "$_id",
-          totalQuantity: { $sum: '$total' },
+    ];
+
+    let filterParams = atob(req.query.filter);
+    filterParams = JSON.parse(filterParams);
+    console.log(req.query.filter);
+    console.log(filterParams);
+
+    let minN = `items.${filterParams?.transactionItemsQuantity?.from}`;
+    let maxN = `items.${filterParams?.transactionItemsQuantity?.to}`;
+
+    filterParams.transactionItemsQuantity &&
+      aggregationPipeline.push({
+        $match: {
+          $and: [{ [minN]: { $exists: true } }, { [maxN]: { $exists: false } }],
         },
-      },
-    ]);
+      });
+
+    filterParams.quantityPerTicketRange &&
+      aggregationPipeline.push({
+        $match: {
+          transactionItemsQuantity: {
+            $gte: filterParams.quantityPerTicketRange.from,
+            $lte: filterParams.quantityPerTicketRange.to,
+          },
+        },
+      });
+
+    filterParams.transactionAmountRange &&
+      aggregationPipeline.push({
+        $match: {
+          transactionAmount: {
+            $gte: filterParams.transactionAmountRange.from,
+            $lte: filterParams.transactionAmountRange.to,
+          },
+        },
+      });
+
+    filterParams.dateRange &&
+      aggregationPipeline.push({
+        $match: {
+          issuedTime: {
+            $gte: new Date(filterParams.dateRange.from),
+            $lte: new Date(filterParams.dateRange.to),
+          },
+        },
+      });
+    filterParams.productArray &&
+      aggregationPipeline.push({
+        $match: {
+          items: {
+            $elemMatch: { productName: { $in: filterParams.productArray } },
+          },
+        },
+      });
+
+    filterParams.priceRange &&
+      aggregationPipeline.push({
+        $match: {
+          transactionAmount: {
+            $gte: filterParams.priceRange.from,
+            $lte: filterParams.priceRange.to,
+          },
+        },
+      });
+    filterParams.customerArray &&
+      aggregationPipeline.push({
+        $match: { "customer.name": { $in: filterParams.customerArray } },
+      });
+
+    const mongooseQuery = Transaction.aggregate([...aggregationPipeline]);
 
     const results = await mongooseQuery;
     res.send({
