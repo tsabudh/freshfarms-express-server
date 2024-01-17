1c8fbbb4c32717270176080645fb78b97c07df3c
tsabudh <tsabudh@gmail.com>
2024-01-17 17:06:24 +0545

    added resize and upload photo middlewares

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -1,8 +1,17 @@
 import { Request, Response, NextFunction } from "express";
-import * as jwt from 'jsonwebtoken';
-import Admin from "../models/Admin";
+import multer from "multer";
+import sharp from "sharp";
+import * as fs from "fs";
 
-export const signupAdmin = async (req: Request, res: Response, next: NextFunction) => {
+import * as jwt from "jsonwebtoken";
+import Admin from "../models/Admin";
+import { s3uploadV2 } from "../utils/s3Service";
+
+export const signupAdmin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     let adminDetails = req.body;
     let newAdmin = new Admin(adminDetails);
@@ -15,13 +24,13 @@ export const signupAdmin = async (req: Request, res: Response, next: NextFunctio
       issuedAt: Date.now(),
     };
 
-    console.log(payload)
+    console.log(payload);
     let token = jwt.sign(payload, "secretKey");
 
     res.send({
       status: "success",
       data: newAdmin,
-      token: token
+      token: token,
     });
   } catch (error: any) {
     res.send({
@@ -31,12 +40,88 @@ export const signupAdmin = async (req: Request, res: Response, next: NextFunctio
   }
 };
 
-export const getMyDetails = async (req: Request, res: Response, next: NextFunction) => {
-  let adminId = (res.locals.currentUser)
+export const getMyDetails = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let adminId = res.locals.currentUser;
   let myDetails = await Admin.findById(adminId);
-  console.log(myDetails)
+  console.log(myDetails);
   res.status(200).json({
-    status: 'success',
-    data: myDetails
-  })
-}
+    status: "success",
+    data: myDetails,
+  });
+};
+
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "src/localData");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const storage = multer.memoryStorage();
+
+const upload = multer({
+  storage,
+});
+
+export const uploadFile = upload.single("photo");
+
+export const uploadPhoto = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    console.log(req.file)
+    let file = req.file;
+
+    let result = await s3uploadV2(file)
+
+    res.json({
+      status: "success",
+      result
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+};
+export const resizeImage = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    if (!req.file) return next(new Error('File not provided!'));
+
+    if (!req.file.mimetype.startsWith('image')) return next(new Error('Please provide an image file.'))
+
+    req.file.mimetype = 'image/webp';
+    req.file.originalname = 'userphoto.webp';
+
+    console.log(req.file);
+    // const buf = await fs.promises.readFile(req.file.path);
+
+    req.file.buffer = await sharp(req.file.buffer)
+      .resize({ height: 500, width: 500, kernel: sharp.kernel.nearest })
+      // .toFormat("webp")
+      .webp({ quality: 90 })
+      .toBuffer()
+
+
+
+    next();
+  } catch (error) {
+
+    console.log(error);
+    res.status(400).json({
+      status: 'failure',
+      message: error.message
+    })
+  }
+};
