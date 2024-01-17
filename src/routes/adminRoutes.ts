30d5001963970ff7bdaadca9b46ab7ef4484393e
tsabudh <tsabudh@gmail.com>
2024-01-17 17:06:59 +0545

    added route uploadphoto

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -10,10 +10,15 @@ const router = express.Router();
 router.route("/signup").post(validateAdminDetails(false), checkValidationErrors, cleanCache, adminController.signupAdmin);
 
 router.route("/getMyDetails").get(authController.checkClearance, adminController.getMyDetails)
+//! NOT DONE YET!! Include clearance
+
+router.route("/upload")
+  .post(adminController.uploadFile, adminController.resizeImage, adminController.uploadPhoto);
 
 router
   .route("/login")
   .post(authController.validateAccount, authController.loginAccount);
+
 
 router.route("/logout").post(authController.logoutAccount);
 
