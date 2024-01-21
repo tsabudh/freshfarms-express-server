29cbaf3f06668c32dcf8b8e3a0007f1237356152
tsabudh <tsabudh@gmail.com>
2024-01-21 09:19:14 +0545

    removed logs

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,11 @@ import app from "./app";
 import path from "path";
 
 dotenv.config({ path: path.join(__dirname + "../../.env") });
-// console.log(process.env);
 const mongoDB: string = (process.env.DATABASE_STRING as string).replace(
   "<password>",
   process.env.DATABASE_PASSWORD as string
 );
+
 // const mongoDB: string = "mongodb://127.0.0.1:27017/shree-krishna";
 // const redisURL: string = "redis://127.0.0.1:6379";
 
@@ -26,12 +26,11 @@ async function connectDatabase() {
     const client = redis.createClient({
       password: 'TBNhJVR63wtELcoCbGVPK9vzlsSyyZNT',
       socket: {
-          host: 'redis-16113.c325.us-east-1-4.ec2.cloud.redislabs.com',
-          port: 16113
+        host: 'redis-16113.c325.us-east-1-4.ec2.cloud.redislabs.com',
+        port: 16113
       }
-  });    await client.connect();
+    }); await client.connect();
 
-    // await client.flushAll();
 
     console.log("Connected to Redis.");
   } catch (error) {
@@ -40,8 +39,8 @@ async function connectDatabase() {
   }
 }
 
-// Leftout from HTTPS implementation
-const server =  app;
+// Left out from HTTPS implementation
+const server = app;
 
 server.listen(3000, () => { console.log('Server started on port 3000') });
 connectDatabase();
