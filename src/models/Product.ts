86a56093e7867fbe5528641050d944d533e4c2e6
tsabudh <tsabudh@gmail.com>
2024-02-12 14:36:06 +0545

    added code,unit to product

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
-  name: { type: String, lowercase: true, required: true },
-
+  name: { type: String, lowercase: true, required: true, unique: true },
+  unit: { type: String, enum: ['kg', 'scoop', 'litre'], required: true },
+  code: { type: String, lowercase: true, required: true, maxLength: 3 },
   price: { type: Number, required: true },
 
   stock: { type: Number, required: true, default: 0 },
