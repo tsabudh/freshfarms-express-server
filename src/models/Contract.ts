e5389bf22496c933c8ad6a121d9d8392f9a417d8
tsabudh <tsabudh@gmail.com>
2024-03-08 21:32:17 +0545

    added model for contracts

---
+++ src/models/Contract.ts
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+
+import Product from './Product';
+import Customer from './Customer';
+import AppError from '../utils/appError';
+
+
+
+interface IContract {
+    customerId: mongoose.Schema.Types.ObjectId;
+    items: Array<{
+        quantity: Number;
+        price: Number;
+
+    }>;
+    interval: Number;
+}
+
+const contractSchema = new mongoose.Schema<IContract>({
+    customerId: {
+        type: mongoose.Schema.ObjectId, ref: "Customer"
+
+    },
+    items: [
+        {
+            productId: mongoose.Schema.ObjectId,
+            ref: "Product",
+            quantity: Number,
+            price: Number,
+        }
+    ],
+    interval: {
+        type: Number
+    }
+
+});
+
+const Contract = mongoose.model('Contract', contractSchema);
+
+export default Contract;
