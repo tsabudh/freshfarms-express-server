07527d14d216185841c3b06dbca6b95bcf0a35e3
tsabudh <tsabudh@gmail.com>
2024-03-18 08:41:09 +0545

    added get and post routes

--- src/routes/contractRoutes.ts
+++ src/routes/contractRoutes.ts
@@ -2,6 +2,11 @@ import mongoose from 'mongoose';
 
 import express from 'express';
 
+import { createContract, getAllContracts } from '../controllers/contractController';
+
 const router = express.Router();
 
-router.route('/')
+router.route("/").get(getAllContracts).post(createContract);
+
+
+export default router;
