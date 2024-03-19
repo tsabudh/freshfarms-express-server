b439b2b458db918b51eb6013101de90682ba3d06
tsabudh <tsabudh@gmail.com>
2024-03-19 09:52:31 +0545

    added DELETE method for contracts/:id route

--- src/routes/contractRoutes.ts
+++ src/routes/contractRoutes.ts
@@ -2,11 +2,13 @@ import mongoose from 'mongoose';
 
 import express from 'express';
 
-import { createContract, getAllContracts } from '../controllers/contractController';
+import { createContract, deleteContract, getAllContracts } from '../controllers/contractController';
 
 const router = express.Router();
 
 router.route("/").get(getAllContracts).post(createContract);
+
+router.route("/:id").delete(deleteContract);
 
 
 export default router;
