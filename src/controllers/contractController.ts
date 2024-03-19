9e438041be8fe9ae0ef39da90ee8e0bcc03d9287
tsabudh <tsabudh@gmail.com>
2024-03-19 09:51:43 +0545

    added deleteContract

--- src/controllers/contractController.ts
+++ src/controllers/contractController.ts
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
-import { createOne, getAll } from '../controllers/controllerFactory';
+import { createOne, deleteOne, getAll } from '../controllers/controllerFactory';
 import Contract from '../models/Contract';
 
 export const getAllContracts =  getAll(Contract);
 
 export const createContract = createOne(Contract);
+
+export const deleteContract = deleteOne(Contract);
