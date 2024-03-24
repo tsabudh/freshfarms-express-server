e534e84bbc512cb6d92391391b8a6f60baeb9d43
tsabudh <tsabudh@gmail.com>
2024-03-24 08:47:59 +0545

    added function for marking todays deliveries

--- src/controllers/contractController.ts
+++ src/controllers/contractController.ts
@@ -1,9 +1,31 @@
 import mongoose from 'mongoose';
+import { Request, Response, NextFunction } from 'express';
+
 import { createOne, deleteOne, getAll } from '../controllers/controllerFactory';
 import Contract from '../models/Contract';
+import catchAsync from '../utils/catchAsync';
 
-export const getAllContracts =  getAll(Contract);
+export const getAllContracts = getAll(Contract);
 
 export const createContract = createOne(Contract);
 
 export const deleteContract = deleteOne(Contract);
+
+export const markTodaysDelivery = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    const details = req.body;
+    const toUpdate = details.contracts;
+    console.log(toUpdate);
+    await Contract.updateMany(
+        {
+            _id: { $in: toUpdate }
+        },
+        {
+            $push: { commencedDate: Date.now() }
+        });
+
+
+    res.json({
+        status: 'success'
+    })
+
+});
