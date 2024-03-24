4b89cbec984096fef88a3aafeaece9a01d2d81fb
tsabudh <tsabudh@gmail.com>
2024-03-24 08:57:18 +0545

    added validator for product and customer id

--- src/models/Contract.ts
+++ src/models/Contract.ts
@@ -11,9 +11,11 @@ interface IContract {
     items: Array<{
         quantity: Number;
         price: Number;
+        productId: mongoose.Schema.Types.ObjectId;
 
     }>;
     interval: Number;
+    commencedDate: Array<{}>
 }
 
 const contractSchema = new mongoose.Schema<IContract>({
@@ -38,9 +40,44 @@ const contractSchema = new mongoose.Schema<IContract>({
     },
     interval: {
         type: Number
+    },
+    commencedDate: {
+        type: [
+            {
+                type: Date,
+
+            }
+        ]
     }
 
+
+
 });
+
+
+
+
+contractSchema.pre('save', async function (next) {
+
+    let products = await Product.find().select("name _id price");
+
+    this.items.forEach((item) => {
+        if (item.productId) {
+            let productById = products.find((product) => {
+                return product._id.toString() === item.productId.toString();
+            });
+            if (!productById)
+                throw new Error(`Product with id ${item.productId} not found.`);
+
+            // let productById = await Product.findById(item.productId);
+            //   item.productName = productById.name;
+            //   item.priceThen = productById.price;
+        }
+    })
+    next();
+}
+)
+
 
 const Contract = mongoose.model('Contract', contractSchema);
 
