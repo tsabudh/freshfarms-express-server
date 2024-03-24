d14b24cc426a0acdb8aea421cfbad6e284e627eb
tsabudh <tsabudh@gmail.com>
2024-03-24 08:58:11 +0545

    added deliveryCheck route

--- src/routes/contractRoutes.ts
+++ src/routes/contractRoutes.ts
@@ -2,11 +2,13 @@ import mongoose from 'mongoose';
 
 import express from 'express';
 
-import { createContract, deleteContract, getAllContracts } from '../controllers/contractController';
+import { createContract, deleteContract, getAllContracts, markTodaysDelivery } from '../controllers/contractController';
 
 const router = express.Router();
 
 router.route("/").get(getAllContracts).post(createContract);
+
+router.route("/deliveryCheck").post(markTodaysDelivery);
 
 router.route("/:id").delete(deleteContract);
 
