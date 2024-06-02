8f944a2fa1d3e6644eb90f27017e5fe4a0c95b1c
tsabudh <tsabudh@gmail.com>
2024-06-02 23:42:53 +0545

    Feature: Added message route /getMyMessages for retreiving messages of currently logged in admin

---
+++ src/routes/messageRoutes.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+import * as authController from "../controllers/authController";
+import * as messageController from "../controllers/messageController";
+const router = express.Router();
+
+router.use(authController.checkClearance);
+router.route("/getMyMessages").get(messageController.getMyMessages);
+
+export default router;
